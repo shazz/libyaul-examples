a96fb1638caf011040a581de9d5772175f501bcc
ShazzTRSi <bitbucket@metaverse.fr>
2016-03-15 21:33:37 -0400

    fix blur test

--- blur_test/blur_test.c
+++ blur_test/blur_test.c
@@ -2,14 +2,21 @@
 #include <langam.h>
 #include <stdlib.h>
 
-#include "kingkong.h"
+#include "blurry_256.h"
+#include "blurry_head_256.h"
+
+#define PLANE2_PNT  blurry_256_pattern_name_table
+#define PLANE2_CD   blurry_256_cell_data
+#define PLANE2_CP   blurry_256_cell_palette
+
+#define PLANE0_PNT  blurry_head_256_pattern_name_table
+#define PLANE0_CD   blurry_head_256_cell_data
+#define PLANE0_CP   blurry_head_256_cell_palette
 
 /*
  * VDP2 VRAM Organization
  * Bank A0
- * - NBG3 plane data, from 0x0 to 0x01000 (4096 bytes, 1 word per cell, 512x256px)
- * - NBG3 cell pattern data, from 0x01000 to 0x06860 (22624 bytes, 707 8x8 cells in 4bits color)
- * => total : 26720 bytes instead of 65536 bytes in bitmap mode
+ *
  * 
  * Bank A1
  * - Nothing
@@ -21,7 +28,7 @@
  * - Nothing
  * 
  * CRAM
- * - NBG3 palette set at palette 0 (0x0)
+ * 
  * 
  * Registers:
  * - TVMD: enable display, border color mode to back screen, 224 lines vertical resolution
@@ -34,7 +41,10 @@ static uint32_t tick = 0;
 static void vblank_in_handler(irq_mux_handle_t *);
 static void vblank_out_handler(irq_mux_handle_t *);
 
-static uint16_t *_nbg3_planes[4] = {
+#define NBG0_VRAM_PAL_NB 0
+#define NBG2_VRAM_PAL_NB 1
+
+static uint16_t *_nbg0_planes[4] = {
         /* VRAM A0 */
         (uint16_t *)VRAM_ADDR_4MBIT(0, 0x0),
         /* VRAM A0 */
@@ -45,124 +55,181 @@ static uint16_t *_nbg3_planes[4] = {
         (uint16_t *)VRAM_ADDR_4MBIT(0, 0x0)
 }; // a plane needs 2048 u16 (512x256) or 0x1000 bytes (4096)
 
-/* VRAM A0 after plane */
-static uint32_t *_nbg3_cell_data = (uint32_t *)VRAM_ADDR_4MBIT(0, 0x1000);
-static uint16_t _nbg3_cell_data_number = VDP2_PN_CONFIG_1_CHARACTER_NUMBER((uint32_t)VRAM_ADDR_4MBIT(0, 0x1000));
+static uint16_t *_nbg2_planes[4] = {
+        /* VRAM B0 */
+        (uint16_t *)VRAM_ADDR_4MBIT(2, 0x0),
+        /* VRAM B0 */
+        (uint16_t *)VRAM_ADDR_4MBIT(2, 0x0),
+        /* VRAM B0 */
+        (uint16_t *)VRAM_ADDR_4MBIT(2, 0x0),
+        /* VRAM B0 */
+        (uint16_t *)VRAM_ADDR_4MBIT(2, 0x0)
+}; // a plane needs 2048 u16 (512x256) or 0x1000 bytes (4096)
+
+/* VRAM A0 after plane  */
+static uint32_t *_nbg0_cell_data = (uint32_t *)VRAM_ADDR_4MBIT(0, 0x1000);
+static uint16_t _nbg0_cell_data_number = VDP2_PN_CONFIG_1_CHARACTER_NUMBER((uint32_t)VRAM_ADDR_4MBIT(0, 0x1000));
+
+/* VRAM B0 after plane */
+static uint32_t *_nbg2_cell_data = (uint32_t *)VRAM_ADDR_4MBIT(2, 0x1000);
+static uint16_t _nbg2_cell_data_number = VDP2_PN_CONFIG_1_CHARACTER_NUMBER((uint32_t)VRAM_ADDR_4MBIT(2, 0x1000));
 
 /* CRAM */
-static uint32_t *_nbg3_color_palette = (uint32_t *)CRAM_MODE_0_OFFSET(0, 0, 0);
-static uint16_t _nbg3_palette_number = VDP2_PN_CONFIG_0_PALETTE_NUMBER(CRAM_MODE_0_OFFSET(0, 0, 0));
+static uint32_t *_nbg0_color_palette = (uint32_t *)CRAM_MODE_0_OFFSET(NBG0_VRAM_PAL_NB, 0, 0);
+static uint32_t *_nbg2_color_palette = (uint32_t *)CRAM_MODE_0_OFFSET(NBG2_VRAM_PAL_NB, 0, 0);
+static uint16_t _nbg0_palette_number = VDP2_PN_CONFIG_1_PALETTE_NUMBER(CRAM_MODE_0_OFFSET(NBG0_VRAM_PAL_NB, 0, 0));
+static uint16_t _nbg2_palette_number = VDP2_PN_CONFIG_1_PALETTE_NUMBER(CRAM_MODE_0_OFFSET(NBG2_VRAM_PAL_NB, 0, 0));
 
-static unsigned int joyLeftTrigger = 0, joyRightTrigger = 0;    
+static unsigned int joyLeft = 0, joyRight = 0;    
+static uint8_t g_blur_level = 0;
 
-static void hardware_init(void)
+/*
+ *  void init_scrollscreen_nbg0(void)
+ */
+void init_scrollscreen_nbg0(void)
 {
-	/* VDP2 */
-	vdp2_init();
+    struct scrn_cell_format nbg0_format;
     
-    // Set Color mode to mode 0 (1KWord Color RAM), 2 banks
-    MEMORY_WRITE(16, VDP2(RAMCTL), 0x300);     // 0000 0011 0000 0000    
+	nbg0_format.scf_scroll_screen = SCRN_NBG0;
+	nbg0_format.scf_cc_count = SCRN_CCC_PALETTE_256;
+	nbg0_format.scf_character_size= 1 * 1;
+	nbg0_format.scf_pnd_size = 1; /* 1 word */
+	nbg0_format.scf_auxiliary_mode = 1;
+	nbg0_format.scf_cp_table = (uint32_t)_nbg0_cell_data;
+	nbg0_format.scf_color_palette = (uint32_t)_nbg0_color_palette;
+	nbg0_format.scf_plane_size = 1 * 1;
+	nbg0_format.scf_map.plane_a = (uint32_t)_nbg0_planes[0];
+	nbg0_format.scf_map.plane_b = (uint32_t)_nbg0_planes[1];
+	nbg0_format.scf_map.plane_c = (uint32_t)_nbg0_planes[2];
+	nbg0_format.scf_map.plane_d = (uint32_t)_nbg0_planes[3];
 
-	/* VDP1 */
-	vdp1_init();
+	vdp2_scrn_cell_format_set(&nbg0_format);
 
-	/* SMPC */
-	smpc_init();
-	smpc_peripheral_init();
+    /* Build the pattern data */   
+    uint32_t i;
+    uint16_t *nbg0_page0 = _nbg0_planes[0];
 
-	/* Disable interrupts */
-	cpu_intc_disable();
+	for (i = 0; i < 2048; i++) {
+			uint16_t cell_data_number = _nbg0_cell_data_number + PLANE0_PNT[i];
+			nbg0_page0[i] = cell_data_number | _nbg0_palette_number;
+	}
 
-	irq_mux_t *vblank_in;
-	irq_mux_t *vblank_out;
-
-	vblank_in = vdp2_tvmd_vblank_in_irq_get();
-	irq_mux_handle_add(vblank_in, vblank_in_handler, NULL);
-
-	vblank_out = vdp2_tvmd_vblank_out_irq_get();
-	irq_mux_handle_add(vblank_out, vblank_out_handler, NULL);
-
-	/* Enable interrupts */
-	cpu_intc_enable();
-    
-	// Enable color cal on NBG3  only: BOKEN | BOKN2 | BOKN1
-    MEMORY_WRITE(16, VDP2(CCCTL), (1 << 15) | (6 << 12));    	
+	vdp2_priority_spn_set(SCRN_NBG0, 7);
+    vdp2_scrn_display_set(SCRN_NBG0, /* transparent = */ true);    
 }
 
-void init_scrollscreen_nbg3(void)
+/*
+ *  void init_scrollscreen_nbg2(void)
+ */
+void init_scrollscreen_nbg2(void)
 {
-    /* We want to be in VBLANK */
-    vdp2_tvmd_display_clear();
+	/* NBG2 stuff is located in B0 bank */
 
-    /* set NBG3 in cell mode, 16 col, 1x1 */
-    struct scrn_cell_format nbg3_format;
+    /* set NBG2 in cell mode, 256 col, 1x1 */
+    struct scrn_cell_format nbg2_format;
+
+	nbg2_format.scf_scroll_screen = SCRN_NBG2;
+	nbg2_format.scf_cc_count = SCRN_CCC_PALETTE_256;
+	nbg2_format.scf_character_size= 1 * 1;
+	nbg2_format.scf_pnd_size = 1; /* 1 word */
+	nbg2_format.scf_auxiliary_mode = 1;
+	nbg2_format.scf_cp_table = (uint32_t)_nbg2_cell_data;
+	nbg2_format.scf_color_palette = (uint32_t)_nbg2_color_palette;
+	nbg2_format.scf_plane_size = 1 * 1;
+	nbg2_format.scf_map.plane_a = (uint32_t)_nbg2_planes[0];
+	nbg2_format.scf_map.plane_b = (uint32_t)_nbg2_planes[1];
+	nbg2_format.scf_map.plane_c = (uint32_t)_nbg2_planes[2];
+	nbg2_format.scf_map.plane_d = (uint32_t)_nbg2_planes[3];
+
+	vdp2_scrn_cell_format_set(&nbg2_format);    
+
+    /* Build the pattern data */   
+    uint32_t i;
+    uint16_t *nbg2_page0 = _nbg2_planes[0];
+
+	for (i = 0; i < 2048; i++) {
+			uint16_t cell_data_number = _nbg2_cell_data_number + PLANE2_PNT[i];
+			nbg2_page0[i] = cell_data_number | _nbg2_palette_number;
+	}
+
+	vdp2_priority_spn_set(SCRN_NBG2, 6);
+    vdp2_scrn_display_set(SCRN_NBG2, /* transparent = */ false);
+}
+
+/*
+ *  void set_VRAM_access(void)
+ *  Set VRAM access priorities
+ */
+void set_VRAM_access(void)
+{
     struct vram_ctl *vram_ctl;
-
-    /* We want to be in VBLANK-IN (retrace) */
-    vdp2_tvmd_display_clear();
-
-	nbg3_format.scf_scroll_screen = SCRN_NBG3;
-	nbg3_format.scf_cc_count = SCRN_CCC_PALETTE_16;
-	nbg3_format.scf_character_size= 1 * 1;
-	nbg3_format.scf_pnd_size = 1; /* 1 word */
-	nbg3_format.scf_auxiliary_mode = 1;
-	nbg3_format.scf_cp_table = (uint32_t)_nbg3_cell_data;
-	nbg3_format.scf_color_palette = (uint32_t)_nbg3_color_palette;
-	nbg3_format.scf_plane_size = 1 * 1;
-	nbg3_format.scf_map.plane_a = (uint32_t)_nbg3_planes[0];
-	nbg3_format.scf_map.plane_b = (uint32_t)_nbg3_planes[1];
-	nbg3_format.scf_map.plane_c = (uint32_t)_nbg3_planes[2];
-	nbg3_format.scf_map.plane_d = (uint32_t)_nbg3_planes[3];
-
-	vdp2_scrn_cell_format_set(&nbg3_format);
-    vdp2_priority_spn_set(SCRN_NBG3, 2);
-
+    
+    // 16 colors bitmap requires 1 access each
     vram_ctl = vdp2_vram_control_get();
-    vram_ctl->vram_cycp.pt[0].t7 = VRAM_CTL_CYCP_CHPNDR_NBG3;
-    vram_ctl->vram_cycp.pt[0].t6 = VRAM_CTL_CYCP_PNDR_NBG3;
-    vram_ctl->vram_cycp.pt[0].t5 = VRAM_CTL_CYCP_NO_ACCESS;
+	
+    // Rules:
+    // only 2 PNDR per timing and split accros x0 and x1 banks
+    // CHPNDR cannot be acceded during certain cycles after PNDR
+    
+	// Bank A0, 256 colors requires 2 access each
+    vram_ctl->vram_cycp.pt[0].t7 = VRAM_CTL_CYCP_PNDR_NBG0; 	// NBG0 character pattern or bitmap data read
+    vram_ctl->vram_cycp.pt[0].t6 = VRAM_CTL_CYCP_CHPNDR_NBG0; 
+    vram_ctl->vram_cycp.pt[0].t5 = VRAM_CTL_CYCP_CHPNDR_NBG0; 
     vram_ctl->vram_cycp.pt[0].t4 = VRAM_CTL_CYCP_NO_ACCESS;
     vram_ctl->vram_cycp.pt[0].t3 = VRAM_CTL_CYCP_NO_ACCESS;
     vram_ctl->vram_cycp.pt[0].t2 = VRAM_CTL_CYCP_NO_ACCESS;
     vram_ctl->vram_cycp.pt[0].t1 = VRAM_CTL_CYCP_NO_ACCESS;
     vram_ctl->vram_cycp.pt[0].t0 = VRAM_CTL_CYCP_NO_ACCESS;
-    vdp2_vram_control_set(vram_ctl);
 
-    /* Copy the palette data */
-    memcpy(_nbg3_color_palette, kingkong_cell_palette, sizeof(kingkong_cell_palette));
-
-    /* Copy the cell data */
-    memcpy(_nbg3_cell_data, kingkong_cell_data, sizeof(kingkong_cell_data));
-
-    /* Build the pattern data */   
-    uint32_t i;
-    uint16_t *nbg3_page0 = _nbg3_planes[0];
-
-	for (i = 0; i < 2048; i++) {
-			uint16_t cell_data_number = _nbg3_cell_data_number + kingkong_pattern_name_table[i];
-			nbg3_page0[i] = cell_data_number | _nbg3_palette_number;
-	}
-
-	// blur doesn't work in scroll screen is transparent
-    vdp2_scrn_display_set(SCRN_NBG3, /* transparent = */ false);
-    MEMORY_WRITE(16, VDP2(BGON), (1 << 3) | (1 << 11));
+	// Bank A1
+    vram_ctl->vram_cycp.pt[1].t7 = VRAM_CTL_CYCP_NO_ACCESS; 	// NBG1 character pattern or bitmap data read
+    vram_ctl->vram_cycp.pt[1].t6 = VRAM_CTL_CYCP_NO_ACCESS;     // NBG1 pattern name data read
+    vram_ctl->vram_cycp.pt[1].t5 = VRAM_CTL_CYCP_NO_ACCESS;
+    vram_ctl->vram_cycp.pt[1].t4 = VRAM_CTL_CYCP_NO_ACCESS;
+    vram_ctl->vram_cycp.pt[1].t3 = VRAM_CTL_CYCP_NO_ACCESS;
+    vram_ctl->vram_cycp.pt[1].t2 = VRAM_CTL_CYCP_NO_ACCESS;
+    vram_ctl->vram_cycp.pt[1].t1 = VRAM_CTL_CYCP_NO_ACCESS;
+    vram_ctl->vram_cycp.pt[1].t0 = VRAM_CTL_CYCP_NO_ACCESS;
+	
+	// Bank B0
+    vram_ctl->vram_cycp.pt[2].t7 = VRAM_CTL_CYCP_NO_ACCESS; 	// NBG2 character pattern data read
+    vram_ctl->vram_cycp.pt[2].t6 = VRAM_CTL_CYCP_NO_ACCESS; 	// NBG2 pattern name data read
+    vram_ctl->vram_cycp.pt[2].t5 = VRAM_CTL_CYCP_NO_ACCESS;
+    vram_ctl->vram_cycp.pt[2].t4 = VRAM_CTL_CYCP_PNDR_NBG2;
+    vram_ctl->vram_cycp.pt[2].t3 = VRAM_CTL_CYCP_CHPNDR_NBG2;
+    vram_ctl->vram_cycp.pt[2].t2 = VRAM_CTL_CYCP_CHPNDR_NBG2;
+    vram_ctl->vram_cycp.pt[2].t1 = VRAM_CTL_CYCP_NO_ACCESS;
+    vram_ctl->vram_cycp.pt[2].t0 = VRAM_CTL_CYCP_NO_ACCESS;	
+    
+	// Bank B1
+    vram_ctl->vram_cycp.pt[3].t7 = VRAM_CTL_CYCP_NO_ACCESS;
+    vram_ctl->vram_cycp.pt[3].t6 = VRAM_CTL_CYCP_NO_ACCESS;
+    vram_ctl->vram_cycp.pt[3].t5 = VRAM_CTL_CYCP_NO_ACCESS;
+    vram_ctl->vram_cycp.pt[3].t4 = VRAM_CTL_CYCP_NO_ACCESS;
+    vram_ctl->vram_cycp.pt[3].t3 = VRAM_CTL_CYCP_NO_ACCESS;
+    vram_ctl->vram_cycp.pt[3].t2 = VRAM_CTL_CYCP_NO_ACCESS;
+    vram_ctl->vram_cycp.pt[3].t1 = VRAM_CTL_CYCP_NO_ACCESS;
+    vram_ctl->vram_cycp.pt[3].t0 = VRAM_CTL_CYCP_NO_ACCESS;	    
+	
+    vdp2_vram_control_set(vram_ctl);   
 }
 
 void read_digital_pad(void)
 {
 	if (g_digital.connected == 1)
 	{
-		joyRightTrigger = g_digital.pressed.button.r;                            
-		joyLeftTrigger = g_digital.pressed.button.l;
+		joyRight = g_digital.pressed.button.right;                            
+		joyLeft = g_digital.pressed.button.left;
 
-        // Enable color cal on NBG3 only => CCCTL{ BOKEN | BOKN2 | BOKN1 }
-		if (joyRightTrigger)
+		if (joyRight)
 		{
-				MEMORY_WRITE(16, VDP2(CCCTL), (1 << 15) | (6 << 12));
+            if(g_blur_level < 0x1F) g_blur_level++;
+            MEMORY_WRITE(16, VDP2(CCRNB), g_blur_level & 0x1F);
 		}
-		else if (joyLeftTrigger)
+		else if (joyLeft)
 		{
-				MEMORY_WRITE(16, VDP2(CCCTL), 0x0);	  			
+           if(g_blur_level > 0x0) g_blur_level--;
+            MEMORY_WRITE(16, VDP2(CCRNB), g_blur_level & 0x1F);
 		}
 		
 		// exit
@@ -170,12 +237,66 @@ void read_digital_pad(void)
 	}  
 }
 
+/*
+ * Public Functions 
+ * 
+ */
+void init(void)
+{           
+    /* We want to be in VBLANK-IN (retrace) */
+    vdp2_tvmd_display_clear();    
+        
+    // Set Color mode to mode 0 (1KWord Color RAM), 2 banks
+    MEMORY_WRITE(16, VDP2(RAMCTL), 0x300);     
+    
+	// Enable gradiation on NG2 and enable color fonction on NGB2
+    MEMORY_WRITE(16, VDP2(CCCTL), (1 << 15) | (1 << 14) | (1 << 12) | 0x4);    
+    MEMORY_WRITE(16, VDP2(CCRNB), 0x0);
+    
+    
+    /* DMA Indirect list, aligned on 64 bytes due to more than 24bytes size (6*4*3=72) */
+    uint32_t dma_tbl[] __attribute__((aligned(64))) = { 
+            (uint32_t)sizeof(PLANE0_CD), (uint32_t)_nbg0_cell_data, (uint32_t)PLANE0_CD, 
+            (uint32_t)sizeof(PLANE0_CP), (uint32_t)_nbg0_color_palette, (uint32_t)PLANE0_CP, 
+            (uint32_t)sizeof(PLANE2_CD), (uint32_t)_nbg2_cell_data, (uint32_t)PLANE2_CD, 
+            (uint32_t)sizeof(PLANE2_CP), (uint32_t)_nbg2_color_palette, (uint32_t)PLANE2_CP                      
+    };    
+    scu_dma_listcpy(dma_tbl, 4*3);
+    while(scu_dma_get_status(SCU_DMA_ALL_CH) == SCU_DMA_STATUS_WAIT);
+    
+    /* set all other stuff */ 
+    init_scrollscreen_nbg0();
+	init_scrollscreen_nbg2();
+	set_VRAM_access();
+}
+
 int main(void)
 {
-	static uint16_t back_screen_color = { COLOR_RGB_DATA | COLOR_RGB555(0, 0, 0) };
-	
-	hardware_init();
-    init_scrollscreen_nbg3();
+    irq_mux_t *vblank_in;
+    irq_mux_t *vblank_out;
+    
+    static uint16_t back_screen_color = { COLOR_RGB_DATA | COLOR_RGB555(0, 0, 0) };
+
+    vdp2_init();
+    
+    vdp1_init();
+
+    smpc_init();
+    
+    smpc_peripheral_init();
+    
+    scu_dma_cpu_init();
+
+    cpu_intc_disable();
+    vblank_in = vdp2_tvmd_vblank_in_irq_get();
+    irq_mux_handle_add(vblank_in, vblank_in_handler, NULL);
+    vblank_out = vdp2_tvmd_vblank_out_irq_get();
+    irq_mux_handle_add(vblank_out, vblank_out_handler, NULL);
+    cpu_intc_enable();
+
+    init();
+
+    vdp2_tvmd_display_set();     
 
 	vdp2_scrn_back_screen_color_set(VRAM_ADDR_4MBIT(3, 0x1FFFE), back_screen_color);    
 
